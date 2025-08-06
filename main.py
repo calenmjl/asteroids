6cfac21c7867f6a28782c3bb2243985d2c18f381
calenmjl <calenmjl@outlook.com>
2025-08-05 17:48:40 -0700

    Implement game loop with screen update and quit handling

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
-
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()  # Update the display
+        
 if __name__ == "__main__":
     main()
